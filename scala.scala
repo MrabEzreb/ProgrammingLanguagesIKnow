401b7d15d86791e02c837a2b703af3f2bf5bd330
MrabEzreb <de013b3f@opayq.com>
2015-04-19 10:42:44 -0400

    Added scala, also gotta update the full list file :)

---
+++ scala.scala
@@ -0,0 +1,6 @@
+println("I know scala!")
+println("Scala is a really kewl language")
+println("It compiles into jvm bytecode")
+println("What does that mean? It means that scala and java can be combined.")
+println("You can compile the whole shizaam with the scala compiler, and then you can package into a jar file")
+println("I use the scala-ide plugin for eclipse to automatically use the correct compiler when packaging a jar")
